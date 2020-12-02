11bf354efe339a9c28062149c0dc8ed6dffbdafb
Alexander Kirsanov <alexander.a.kirsanov@gmail.com>
2020-12-03 00:29:19 +0500

    Пример ОПЗ

---
+++ AdvancedFunctionBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdvancedFunctionBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Function("(1/3+0.5)+2*(4-1/2)").ToString());
+        }
+    }
+}
